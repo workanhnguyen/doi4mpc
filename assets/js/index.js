6ec9f828a705a173f60036e74829852644fd823c
anhnguyenvan0910@gmail.com <anhnguyenvan0910@gmail.com>
2022-03-20 08:33:36 +0700

    upgrade file

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+var turnSignUp = document.querySelector('#sign-up');
+var turnSignIn = document.querySelector('#sign-in');
+var modal = document.querySelector('.main__item-modal');
+var turnSign = document.querySelector('.main__item-turn');
+
+turnSignUp.addEventListener('click', function () {
+    modal.classList.add('turn');
+});
+
+turnSignIn.addEventListener('click', function () {
+    modal.classList.remove('turn');
+});
