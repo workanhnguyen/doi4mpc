8ee19ba24682944737e4fb5478fac91b3784326d
anhnguyenvan0910@gmail.com <anhnguyenvan0910@gmail.com>
2022-03-20 09:48:42 +0700

    upgrade files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 // Turn between sign up and sign in
-var turnSignUp = document.querySelector('#sign-up');
-var turnSignIn = document.querySelector('#sign-in');
+var turnSignUp = document.querySelector('#turn-sign-up');
+var turnSignIn = document.querySelector('#turn-sign-in');
 var modal = document.querySelector('.main__item-modal');
 var turnSign = document.querySelector('.main__item-turn');
 
@@ -12,3 +12,31 @@ turnSignIn.addEventListener('click', function () {
     modal.classList.remove('turn');
 });
 
+// Sign in and Sign up
+var signIn = document.querySelector('#sign-in');
+var signUp = document.querySelector('#sign-up');
+var userName;
+var passWord;
+var confirmPassWord;
+
+signIn.addEventListener('click', function () {
+    userName = document.querySelector('.main__item--sign-in > .main__item-input.username').value;
+    passWord = document.querySelector('.main__item--sign-in > .main__item-input.password').value;
+    if (userName === "" || passWord === "") {
+        document.querySelector('p.toast__content-body').innerText = "Please enter all fields!";  
+    }
+    console.log(userName, passWord);
+})
+
+signUp.addEventListener('click', function () {
+    userName = document.querySelector('.main__item--sign-up > .main__item-input.username').value;
+    passWord = document.querySelector('.main__item--sign-up > .main__item-input.password').value;
+    confirmPassWord = document.querySelector('.main__item--sign-up > .main__item-input.confirm-password').value;
+    if (userName === "" || passWord === "" || confirmPassWord === "") {
+        document.querySelector('p.toast__content-body').innerText = "Please enter all fields!";  
+    }
+    if (passWord !== confirmPassWord) {
+        document.querySelector('p.toast__content-body').innerText = "Confirm password not match!";  
+    }
+    console.log(userName, passWord, confirmPassWord);
+})
